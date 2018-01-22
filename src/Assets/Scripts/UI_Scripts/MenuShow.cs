45e6e39273c2edaba7db5df1801f1fc47cfa1e01
JUMANAH AL ASADI <jumanahalasadi@gmail.com>
2018-01-22 16:06:40 -0500

    merged with lena, prototype ready :)

---
+++ src/Assets/Scripts/UI_Scripts/MenuShow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuShow : MonoBehaviour {
+
+    public GameObject Panel;
+    int counter;
+
+    public void showhidePanel()
+    {
+        counter++;
+        if (counter % 2 == 1)
+        {
+            Panel.gameObject.SetActive(true);
+        }
+        else {
+            Panel.gameObject.SetActive(false);
+        }
+    }
+}

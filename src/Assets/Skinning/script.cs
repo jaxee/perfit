fc350c298f4471927033906af1ad6c26e899438a
JUMANAH AL ASADI <jumanahalasadi@gmail.com>
2018-02-14 21:03:26 -0500

    added MeshSkinner demo scene to project

---
+++ src/Assets/Skinning/script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Winterdust;
+
+public class script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject modelGO = GameObject.Find("blackBodycon_small");
+		GameObject skeletonGO = GameObject.Find("Rig");
+
+		MeshSkinner ms = new MeshSkinner(modelGO, skeletonGO);
+		ms.work();
+		ms.finish();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

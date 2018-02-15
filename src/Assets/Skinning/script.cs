9275c30945da1bc0562574ab506aa5902de62542
JUMANAH AL ASADI <jumanahalasadi@gmail.com>
2018-02-14 22:46:58 -0500

    Skinning kinda works in unity ity but the cloth doesnt :-)

--- src/Assets/Skinning/script.cs
+++ src/Assets/Skinning/script.cs
@@ -7,12 +7,13 @@ public class script : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		Debug.Log ("HI");
 		GameObject modelGO = GameObject.Find("blackBodycon_small");
 		GameObject skeletonGO = GameObject.Find("Rig");
-
+		Debug.Log (skeletonGO);
 		MeshSkinner ms = new MeshSkinner(modelGO, skeletonGO);
 		ms.work();
-		ms.finish();
+	    ms.finish();
 	}
 	
 	// Update is called once per frame

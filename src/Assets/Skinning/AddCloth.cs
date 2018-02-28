b4a9cc524136e205622834f58406083d5ada7a94
JUMANAH AL ASADI <jumanahalasadi@gmail.com>
2018-02-28 14:50:29 -0500

    CLOTH WERK WERK WERK

---
+++ src/Assets/Skinning/AddCloth.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class AddCloth : MonoBehaviour
+{
+	public GameObject go1;
+	public GameObject go2;
+	public void addClothToMe(string clothingName)
+	{
+		//https://answers.unity.com/questions/530178/how-to-get-a-component-from-an-object-and-add-it-t.html
+		go1 = GameObject.Find (clothingName);
+		go2 = GameObject.Find ("SkinnedVersion");
+
+			var t1 = go1.GetComponent<Cloth>();
+			var t2 = go2.GetComponent<Cloth>();
+			var temp = new GameObject("_TEMP").AddComponent<Cloth>();
+		    temp = t1;
+			//t1 = t2;
+			t2 = temp;
+			if (UnityEditorInternal.ComponentUtility.CopyComponent(t1)) {
+				if (UnityEditorInternal.ComponentUtility.PasteComponentValues(temp)) {
+					if (UnityEditorInternal.ComponentUtility.CopyComponent(t2)) {
+						if (UnityEditorInternal.ComponentUtility.PasteComponentValues(t1)) {
+							if (UnityEditorInternal.ComponentUtility.CopyComponent(temp)) {
+								if (UnityEditorInternal.ComponentUtility.PasteComponentValues(t2)) {
+								Debug.Log("DONE");
+								}
+							}
+						}
+					}
+				}
+			}
+			Destroy(temp.gameObject);
+
+	}
+
+}
--- src/Assets/Skinning/script.cs
+++ src/Assets/Skinning/script.cs
@@ -5,19 +5,45 @@ using Winterdust;
 
 public class script : MonoBehaviour {
 
+	public AddCloth addClothScript;
+	public GameObject finalProduct;
+	public Cloth clothComponent;
+	public GameObject cube;
 	// Use this for initialization
 	void Start () {
 		Debug.Log ("HI");
-		GameObject modelGO = GameObject.Find("blackBodycon_small");
+		GameObject modelGO = GameObject.Find("small_blueDress_noInside");
 		GameObject skeletonGO = GameObject.Find("Rig");
+		GameObject cube  = GameObject.Find("PinningCube");
 		Debug.Log (skeletonGO);
 		MeshSkinner ms = new MeshSkinner(modelGO, skeletonGO);
 		ms.work();
 	    ms.finish();
+
+		finalProduct = GameObject.Find ("SkinnedVersion");
+		finalProduct.AddComponent<Cloth> ();
+
+		clothComponent = finalProduct.GetComponent<Cloth> ();
+		clothComponent.enabled = false;
+
+		ClothSkinningCoefficient[] newConstraints; 
+		newConstraints = clothComponent.coefficients;
+
+		for (int i = 0; i < clothComponent.vertices.Length; i++) {
+			float dist = Vector3.Distance(clothComponent.vertices[i], cube.transform.position);
+			Debug.Log (dist);
+			if (dist > 425) {
+				newConstraints [i].maxDistance = 0;
+			}
+		}
+		//newConstraints[0].maxDistance = 0;
+
+		clothComponent.coefficients = newConstraints;
+		clothComponent.enabled = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }

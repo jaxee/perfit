2953d9e8d6c57d0f1a548d2becaccf59add67037
JUMANAH AL ASADI <jumanahalasadi@gmail.com>
2018-03-13 01:11:01 -0400

    cloth latest for luk

---
+++ src/Assets/Scripts/AddToBag.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddToBag : MonoBehaviour {
+
+	public GameObject model;
+	public GameObject AddBtn;
+	public string dressNo;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void onClickAdd() {
+		Debug.Log ("Hi");
+		dressNo = this.name;
+		Debug.Log (dressNo);
+
+	}
+}

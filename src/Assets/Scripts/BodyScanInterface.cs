44bddb5f60fba0e67fb90ebc3ccab6dad555179f
Jackie Ellis <jackie.ellis@hotmail.com>
2018-03-22 15:19:34 -0400

    Body scanning interface update

---
+++ src/Assets/Scripts/BodyScanInterface.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class BodyScanInterface : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SaveInformation() {
+		Debug.Log ("The values that you entered are: ");
+
+	}
+}

1f13ad774cf4e0c1ce4c7fb6a1fc8f30581352c9
Lukundu Lako <lukundu.lako@gmail.com>
2018-03-25 09:34:08 -0400

    updated save and transition scripts

---
+++ src/Assets/Scripts/StateManager/SaveManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class SaveManager
+{
+		protected string filePath;
+		//private data;
+		
+		//private SaveData
+
+	public SaveManager (string filename)
+	{
+		this.filePath = Application.persistentDataPath + filename;
+	}
+
+
+	public void Save (BodyscanSave.BodyData input)
+	{//save file 
+        this.filePath = input.file; 
+        BinaryFormatter bin = new BinaryFormatter();
+		FileStream file = File.Create(this.filePath);
+		bin.Serialize(file, input);
+		file.Close();
+	}
+
+	public void Load (BodyscanSave.BodyData input)
+	{//load file
+        this.filePath = input.file;
+        if (File.Exists (this.filePath)) {
+			BinaryFormatter bin = new BinaryFormatter ();
+			FileStream file = File.Open (filePath, FileMode.Open);
+			//data = (Save)bin.Deserialize (file);
+			file.Close ();
+		}
+		//return data;
+	}
+}
+
+

312860be901a2d2c3e2020cd0b74f011d66c4d31
Lukundu Lako <lukundu.lako@gmail.com>
2018-04-02 11:50:40 -0400

    added global UI message script| globalMessange

---
+++ src/Assets/Scripts/StateManager/GlobalMessage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalMessage : UIManager {
+
+	// Update is called once per frame
+	void SendMessage (string msg) {
+        StartCoroutine(MsgModule(msg));
+	}
+}

2c02a80fda0c437ed8f6f4d95cc60406e7678a30
JUMANAH AL ASADI <jumanahalasadi@gmail.com>
2018-04-13 03:35:18 -0400

    adding hair

---
+++ src/Assets/Resources/Hair/AddHair.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Winterdust;
+
+
+public class AddHair : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		if (gameObject.name == "hairBtn1") {
+
+			GameObject newHair = Instantiate (Resources.Load ("Hair/Ponytail")) as GameObject;
+			GameObject parent = GameObject.Find ("QuickRigCharacter3_Head"); 
+
+			newHair.transform.parent = parent.transform;
+
+
+
+//			GameObject modelGO = newHair;
+//			GameObject skeletonGO = GameObject.Find ("QuickRigCharacter3_Reference").gameObject;// Debug.Log (modelGO.name + skeletonGO.name); 
+//
+//			MeshSkinner ms = new MeshSkinner (modelGO, skeletonGO);
+//			ms.work ();
+//			ms.finish ();
+		}
+
+		if (gameObject.name == "hairBtn2") {
+
+			GameObject newHair = Instantiate (Resources.Load ("Hair/Ponytail")) as GameObject;
+			GameObject parent = GameObject.Find ("QuickRigCharacter3_Head"); 
+
+			newHair.transform.parent = parent.transform;
+
+//			GameObject newHair = Instantiate (Resources.Load ("Hair/Afro")) as GameObject;
+//
+//			GameObject modelGO = newHair;
+//			GameObject skeletonGO = GameObject.Find ("QuickRigCharacter3_Reference").gameObject;// Debug.Log (modelGO.name + skeletonGO.name); 
+//
+//			MeshSkinner ms = new MeshSkinner (modelGO, skeletonGO);
+//			ms.work ();
+//			ms.finish ();
+		}
+
+	}
+		
+}
